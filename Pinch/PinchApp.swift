6286771e969189ce0f699457696f14005bd5b00b
smit-sk <patelsmit7008@gmail.com>
2024-02-14 16:51:04 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by SMIT KHOKHARIYA on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
